2192246711b1609942dbfdceca24ba485d52e324
Jasjeet <jasjeet13.saini@gmail.com>
2024-10-07 14:05:12 +0530

    hashtablewithchaining

---
+++ HashTable/TableWithChaining.java
@@ -0,0 +1,67 @@
+package HashTable;
+import java.util.*;;
+
+public class TableWithChaining {
+    public LinkedList<Node>[] Table;
+    public int size;
+
+    static class Node{
+        int key;
+        String value;
+
+        public Node(int key , String value){
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+
+    public TableWithChaining(int size){
+        this.size = size;
+        Table = new LinkedList[size];
+        for(int i=0;i<size;i++){
+            Table[i] = new LinkedList<>();
+
+        }
+    }
+
+    public int hash(int key){
+        return key% this.size;
+    }
+
+    public void put(int key , String value){
+        int index = hash(key);
+
+        for(Node n : Table[index]){
+            if(n.key == key){
+                n.value = value;
+                return ;
+            }
+        }
+
+        Node entry = new Node(key,value);
+        Table[index].add(entry);
+    }
+
+    public String get(int key){
+        int index = hash(key);
+        for(Node n : Table[index]){
+            if(n.key == key){
+                return n.value;
+            }
+        }
+
+        return "Value not found";
+    }
+
+    public static void main(String[] args) {
+        TableWithChaining hashTable = new TableWithChaining(10);
+        hashTable.put(123,"Apple");
+        hashTable.put(245,"Kiwi");
+        hashTable.put(243,"Orange");
+
+        System.out.println(hashTable.get(243));
+        System.out.println(hashTable.get(145));
+    }
+
+}
