8e530fa33bbf754eded7fa92bdc7ff0966b3f5f6
Jasjeet <jasjeet13.saini@gmail.com>
2024-10-09 23:07:55 +0530

    buildingBinaryTree

--- Trees/BuildingBinaryTree.java
+++ Trees/BuildingBinaryTree.java
@@ -1,46 +1,46 @@
-package Trees;
-import java.util.*;
+// package Trees;
+// import java.util.*;
 
-public class BuildingBinaryTree {
-    public Node root;
+// public class BuildingBinaryTree {
+//     public Node root;
 
-    static class Node{
-        int data;
-        Node left;
-        Node right;
+//     static class Node{
+//         int data;
+//         Node left;
+//         Node right;
 
-        Node(int data){
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
+//         Node(int data){
+//             this.data = data;
+//             this.left = null;
+//             this.right = null;
+//         }
+//     }
 
-    public static Node createBinaryTree(){
-       Node root = null;
-       Scanner sc = new Scanner(System.in);
-       System.out.println(" -> Enter value: ");
-       int data = sc.nextInt();
+//     public static Node createBinaryTree(){
+//        Node root = null;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(" -> Enter value: ");
+//        int data = sc.nextInt();
 
-       if(data == -1){
-        return null;
-       }
+//        if(data == -1){
+//         return null;
+//        }
 
-       root = new Node(data);
+//        root = new Node(data);
 
-       System.out.println("Enter value for the left child of " + root.data);
-       root.left = createBinaryTree();
+//        System.out.println("Enter value for the left child of " + root.data);
+//        root.left = createBinaryTree();
 
-       System.out.println("Enter value for the right child of " + root.data);
-       root.right = createBinaryTree();
+//        System.out.println("Enter value for the right child of " + root.data);
+//        root.right = createBinaryTree();
 
-       return root;
+//        return root;
 
 
 
-    }
+//     }
     
-    public static void main(String[] args) {
-        Node root = createBinaryTree();
-    }
-}
+//     public static void main(String[] args) {
+//         Node root = createBinaryTree();
+//     }
+// }
