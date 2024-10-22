e525daaec89b73cea42419639dadb9e665934578
Jasjeet <jasjeet13.saini@gmail.com>
2024-10-22 10:56:45 +0530

    AddingInBst

---
+++ Trees/AddingInBST.java
@@ -0,0 +1,98 @@
+// import java.util.LinkedList;
+// import java.util.Queue;
+
+// public class AddingInBST {
+//     public static class Node{
+//         int data;
+//         Node right;
+//         Node left;
+
+//         Node(int data){
+//             this.data = data;
+//             this.left = null;
+//             this.right = null;
+//         }
+//     }
+
+//     public static Node buildTree(int[] array,int start,int end){
+
+//         if(start>end){
+//             return null;
+//         }
+
+//         int middle = (start+end)/2;
+//         Node root = new Node(array[middle]);
+
+//         root.left = buildTree(array, start, middle-1 );
+//         root.right = buildTree(array, middle+1, end);
+
+//         return root;                   
+//     }
+
+//     public static Node insert(Node root,int n){
+
+//         if(root == null){
+//             return new Node(n);
+//         }
+
+//         if(root.data >= n){
+//             root.left = insert(root.left, n);
+//         }
+
+//         else{
+//             root.right = insert(root.right, n);
+//         }
+
+//         return root;
+//     }
+
+//      public static void levelOrder(Node root){
+//         if(root == null){
+//             return;
+//         }
+
+//         Queue<Node> q = new LinkedList<>();
+//         q.add(root);
+//         q.add(null);
+
+//         while(!q.isEmpty()){
+//             Node curr = q.poll();
+//             if(curr == null){
+//                 System.out.println();
+//                 if(q.isEmpty()){
+//                     break;
+//                 }
+//                 else{
+//                     q.add(null);
+//                 }
+//             }
+//             else{
+//                 System.out.print(curr.data + " ");
+
+//                 if(curr.left != null){
+//                     q.add(curr.left);
+//                 }
+//                 if(curr.right != null){
+//                     q.add(curr.right);
+//                 }
+//             }
+//         }
+
+//     }
+
+//     public static void main(String[] args) {
+//         int[] array = {1, 2, 3, 5, 6, 7}; 
+//         int n = array.length;
+
+//         Node root = buildTree(array, 0, n - 1);
+
+//         System.out.println("Level-order traversal of the Binary Search Tree:");
+//         levelOrder(root);
+        
+//         root = insert(root,4);
+//         System.out.println("Level-order traversal of the Binary Search Tree:");
+//         levelOrder(root);
+
+//     }
+// }
+
--- Trees/BST.java
+++ Trees/BST.java
@@ -2,74 +2,74 @@
 // import java.util.Queue;
 
 // public class BST {
-//     public static class Node{
-//         int data;
-//         Node right;
-//         Node left;
+    // public static class Node{
+    //     int data;
+    //     Node right;
+    //     Node left;
 
-//         Node(int data){
-//             this.data = data;
-//             this.left = null;
-//             this.right = null;
-//         }
-//     }
+    //     Node(int data){
+    //         this.data = data;
+    //         this.left = null;
+    //         this.right = null;
+    //     }
+    // }
 
-//     public static Node buildTree(int[] array,int start,int end){
+    // public static Node buildTree(int[] array,int start,int end){
 
-//         if(start>end){
-//             return null;
-//         }
+    //     if(start>end){
+    //         return null;
+    //     }
 
-//         int middle = (start+end)/2;
-//         Node root = new Node(array[middle]);
+    //     int middle = (start+end)/2;
+    //     Node root = new Node(array[middle]);
 
-//         root.left = buildTree(array, start, middle-1 );
-//         root.right = buildTree(array, middle+1, end);
+    //     root.left = buildTree(array, start, middle-1 );
+    //     root.right = buildTree(array, middle+1, end);
 
-//         return root;                   
-//     }
+    //     return root;                   
+    // }
 
-//     public static void levelOrder(Node root){
-//         if(root == null){
-//             return;
-//         }
+    // public static void levelOrder(Node root){
+    //     if(root == null){
+    //         return;
+    //     }
 
-//         Queue<Node> q = new LinkedList<>();
-//         q.add(root);
-//         q.add(null);
+    //     Queue<Node> q = new LinkedList<>();
+    //     q.add(root);
+    //     q.add(null);
 
-//         while(!q.isEmpty()){
-//             Node curr = q.poll();
-//             if(curr == null){
-//                 System.out.println();
-//                 if(q.isEmpty()){
-//                     break;
-//                 }
-//                 else{
-//                     q.add(null);
-//                 }
-//             }
-//             else{
-//                 System.out.print(curr.data + " ");
+    //     while(!q.isEmpty()){
+    //         Node curr = q.poll();
+    //         if(curr == null){
+    //             System.out.println();
+    //             if(q.isEmpty()){
+    //                 break;
+    //             }
+    //             else{
+    //                 q.add(null);
+    //             }
+    //         }
+    //         else{
+    //             System.out.print(curr.data + " ");
 
-//                 if(curr.left != null){
-//                     q.add(curr.left);
-//                 }
-//                 if(curr.right != null){
-//                     q.add(curr.right);
-//                 }
-//             }
-//         }
+    //             if(curr.left != null){
+    //                 q.add(curr.left);
+    //             }
+    //             if(curr.right != null){
+    //                 q.add(curr.right);
+    //             }
+    //         }
+    //     }
 
-//     }
+    // }
 
 //     public static void main(String[] args) {
-//         int[] array = {1, 2, 3, 4, 5, 6, 7}; 
-//         int n = array.length;
+        // int[] array = {1, 2, 3, 4, 5, 6, 7}; 
+        // int n = array.length;
 
-//         Node root = buildTree(array, 0, n - 1);
+        // Node root = buildTree(array, 0, n - 1);
 
-//         System.out.println("Level-order traversal of the Binary Search Tree:");
-//         levelOrder(root);
+        // System.out.println("Level-order traversal of the Binary Search Tree:");
+        // levelOrder(root);
 //     }
 // }
